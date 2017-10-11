9ac35ac0cf8344e93fc54c617d36f6a01e2cf5bb
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-10-11 18:26:33 +0200

    Handle closing in airbrake error handler

--- errors/airbrake/handler.go
+++ errors/airbrake/handler.go
@@ -3,10 +3,11 @@ package airbrake
 import (
 	"github.com/airbrake/gobrake"
 	"github.com/goph/emperror/airbrake"
+	"github.com/goph/fxt"
 )
 
 // NewHandler returns a new Airbrake handler.
-func NewHandler(config *Config) *airbrake.Handler {
+func NewHandler(config *Config, lifecycle fxt.Lifecycle) *airbrake.Handler {
 	notifier := gobrake.NewNotifier(config.ProjectID, config.ProjectKey)
 
 	if config.Host != "" {
@@ -21,8 +22,14 @@ func NewHandler(config *Config) *airbrake.Handler {
 		notifier.AddFilter(filter)
 	}
 
-	return &airbrake.Handler{
+	handler := &airbrake.Handler{
 		Notifier:          notifier,
 		SendSynchronously: !config.Async,
 	}
+
+	lifecycle.Append(fxt.Hook{
+		OnClose: handler.Close,
+	})
+
+	return handler
 }
